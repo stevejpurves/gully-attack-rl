126904f7381230f8ef9e84db91e59ae901d9cdfe
Tormod Myrvang <tormod.myrvang@gmail.com>
2018-09-19 10:19:49 +0200

    Working octree

--- test.py
+++ test.py
@@ -4,15 +4,16 @@ import time
 import gym
 from test_env import TestEnv
 import numpy as np
+from os import path
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute
+from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute, Input
 from keras.optimizers import Adam
 import keras.backend as K
 
 from rl.agents.dqn import DQNAgent
 from rl.policy import LinearAnnealedPolicy, BoltzmannQPolicy, EpsGreedyQPolicy
-from rl.memory import SequentialMemory
+from rl.memory import SequentialMemory, EpisodeParameterMemory
 from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
@@ -20,40 +21,53 @@ from rl.callbacks import FileLogger, ModelIntervalCheckpoint
     Arguments - setup this script to acept some arguments for training, testing and playback
 '''
 parser = argparse.ArgumentParser()
-parser.add_argument('--mode', choices=['train', 'test', 'random'], default='random')
+parser.add_argument('--mode', choices=['train', 'test'], default='random')
 parser.add_argument('--weights', type=str, default=None)
 args = parser.parse_args()
 
 
 # Setup the environment
+# We use two channels, the first being the grayscale version of the background and the
+# second is used for the hits and misses state
+CHANNELS = 2
 
-IMAGE_POWER = 7
-
-IMAGE_SIZE = 2**IMAGE_POWER
-IMAGE_SHAPE = (IMAGE_SIZE,IMAGE_SIZE)
+IMAGE_SHAPE = (128, 128)
 # its possible as per the keras-rl atari example to preent multiple frames of teh environment
 # to the network to allow teh network to see changes over a short time span
 # setting this to 1 for the first pass, so agent will only make decisions based on the current state
 NUM_FRAMES = 1
 
 # the window applied to look back in sequential memory
-WINDOW_LENGTH = 2
+WINDOW_LENGTH = 4
 
+# the background image (rgba) and the target
 
-env = TestEnv()
+IMAGE_PATH = path.join(path.dirname(__file__), 'images')
+IMAGES = {
+    'wallpaper': 'rgb_il2300.crop.png',
+    'background': 'grey_il2300.crop.png',
+    'target': 'grey_label_il2300.crop.png'
+}
+
+env = TestEnv(image_size=IMAGE_SHAPE[0], 
+                     images=IMAGES, 
+                     image_path=IMAGE_PATH, 
+                     time_limit=100000,
+                     miss_limit=1000)
+
 np.random.seed(42)
 env.seed(42)
+
 nb_actions = env.n_actions
 
 ###
 #
 #   Create the Model that will determine teh next action from the current environment
-#
+
 ###
-CHANNELS = 3
 # This is the network from the keras-rl example
 if args.mode == 'train' or args.mode == 'test':
-    input_shape = (CHANNELS,) + IMAGE_SHAPE
+    input_shape = (WINDOW_LENGTH,) + IMAGE_SHAPE
     model = Sequential()
     if K.image_dim_ordering() == 'tf':
         # (width, height, channels)
@@ -61,9 +75,8 @@ if args.mode == 'train' or args.mode == 'test':
     elif K.image_dim_ordering() == 'th':
         # (channels, width, height)
         model.add(Permute((1, 2, 3), input_shape=input_shape))
-    else:
-        raise RuntimeError('Unknown image_dim_ordering.')
-    model.add(Convolution2D(32, (8, 8), strides=(4, 4)))
+
+    model.add(Convolution2D(32, (8, 8), strides=(4, 4), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
     model.add(Activation('relu'))
@@ -82,7 +95,8 @@ if args.mode == 'train' or args.mode == 'test':
 #
 ###
 if args.mode == 'train' or args.mode == 'test':
-    memory = SequentialMemory(limit=IMAGE_POWER, window_length=WINDOW_LENGTH)
+    memory = SequentialMemory(limit=1000, window_length=WINDOW_LENGTH)
+    #memory = EpisodeParameterMemory(limit=1000, window_length=1)
 
 # Select a policy. We use eps-greedy action selection, which means that a random action is selected
 # with probability eps. We anneal eps from 1.0 to 0.1 over the course of 1M steps. This is done so that
@@ -91,7 +105,7 @@ if args.mode == 'train' or args.mode == 'test':
 # so that the agent still performs some random actions. This ensures that the agent cannot get stuck.
 if args.mode == 'train' or args.mode == 'test':
     policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
-                              nb_steps=IMAGE_POWER*100000)
+                              nb_steps=1000000)
 
 # The trade-off between exploration and exploitation is difficult and an on-going research topic.
 # If you want, you can experiment with the parameters or use a different policy. Another popular one
@@ -104,9 +118,9 @@ if args.mode == 'train' or args.mode == 'test':
         nb_actions=nb_actions,
         policy=policy,
         memory=memory,
-        nb_steps_warmup=IMAGE_POWER*1000,
+        nb_steps_warmup=50000,
         gamma=.99,
-        target_model_update=IMAGE_POWER*100,
+        target_model_update=10000,
         train_interval=4, delta_clip=1.)
     dqn.compile(Adam(lr=.00025), metrics=['mae'])
 
@@ -120,15 +134,15 @@ if args.mode == 'train':
     weights_filename = 'dqn_{}_weights.h5f'.format(timestamp)
     checkpoint_weights_filename = 'dqn_' + str(timestamp) + '_weights_{step}.h5f'
     log_filename = 'dqn_{}_log.json'.format(timestamp)
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=IMAGE_POWER*25000)]
-    callbacks += [FileLogger(log_filename, interval=IMAGE_POWER*10)]
-    dqn.fit(env, callbacks=callbacks, nb_steps=IMAGE_POWER*100000, log_interval=IMAGE_POWER*1000)
+    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
+    callbacks += [FileLogger(log_filename, interval=100)]
+    dqn.fit(env, callbacks=callbacks, nb_steps=1750000, log_interval=10000)
 
     # After training is done, we save the final weights one more time.
     dqn.save_weights(weights_filename, overwrite=True)
 
     # Finally, evaluate our algorithm for 10 episodes.
-    dqn.test(env, nb_episodes=10, visualize=True)
+    dqn.test(env, nb_episodes=10, visualize=False)
 elif args.mode == 'test':
     print("start testing")
     if not args.weights:
--- test_env.py
+++ test_env.py
@@ -4,15 +4,23 @@ from gym.utils import seeding
 import numpy as np
 from os import path
 from PIL import Image
+from math import log2
 
 IMAGE_PATH = path.join(path.dirname(__file__), 'images')
 
-IMAGE_SIZE = 128
-images = {
-    'wallpaper': 'rgb_il2300.crop.png',
-    'background': 'grey_il2300.crop.png',
-    'result': 'grey_label_il2300.crop.png',
+# Maxmimum number of steps before game end
+TIME_LIMIT = 512
+MISS_LIMIT = 10
+
+IMAGE_SIZE = 256
+IMAGES = {
+    'background': 'blob.png',
+    'target': 'blob_gray.png'
 }
+
+def is_power2(num):
+	'states if a number is a power of two'
+	return num != 0 and ((num & (num - 1)) == 0)
 
 class TestEnv(gym.Env):
     """The main OpenAI Gym class. It encapsulates an environment with
@@ -34,40 +42,46 @@ class TestEnv(gym.Env):
         functionality over time.
     """
 
-    def __init__(self, image_power=7):
+    def __init__(self, images, image_path, time_limit=TIME_LIMIT, miss_limit=MISS_LIMIT, image_size=IMAGE_SIZE):
+        self.images = images
+        self.image_path = image_path
+        self.image_size = image_size
+        if not is_power2(self.image_size):
+            raise ValueError("Image size must be a power of 2")
+        self.power = int(log2(image_size))
         if not self._check_paths():
             raise ValueError("Invalid image paths, one or more images don't exist")
 
-        # Load images
-        self.background = self._load_image_as_np(images['background'])
+        self.TIME_LIMIT = time_limit
+        self.MISS_LIMIT = miss_limit
+
+        # The background and target mask is static 
         self.wallpaper = self._load_image_as_np(images['wallpaper'])
-        self.result = self._load_image_as_np(images['result'])
-        self.state = np.zeros_like(self.result)
+        self.background = self._load_image_as_np(images['background'])
+        self.target = self._load_image_as_np(images['target'])
 
-        # Observation space
-        self.observation_space = self.background
+        # (Re)set all counters and state
+        self.reset()
 
-        # Action space
+        # Check that the size is consistent
+        if self.background.shape[0] != self.image_size or self.background.shape[1] != self.image_size:
+            raise ValueError('Incorrect size of background image')
+        if self.target.shape[0] != self.image_size or self.target.shape[1] != self.image_size:
+            raise ValueError('Incorrect size of target image')
+
+        # create an action space for moving and shooting (four directions, one for shooting)
         self._n_actions = 4
-        self._action_space = spaces.Discrete(self.n_actions)
-        self.level = 0
-        self.position = np.zeros(image_power)
-        self.image_power = image_power
-        self.image_size = 2**image_power
-
-        # Reward range
-        #self.reward_range = (-1,1)
+        self._action_space = spaces.Discrete(self._n_actions)
 
         self.viewer = None
 
     def _check_paths(self):
-        print("background", path.join(IMAGE_PATH, images['background']))
-        print("wallpaper", path.join(IMAGE_PATH, images['wallpaper']))
-        print("result", path.join(IMAGE_PATH, images['result']))
-        return path.exists(path.join(IMAGE_PATH, images['background'])) and path.exists(path.join(IMAGE_PATH, images['result'])) and path.exists(path.join(IMAGE_PATH, images['wallpaper']))
+        print("background", path.join(self.image_path, self.images['background']))
+        print("target", path.join(self.image_path, self.images['target']))
+        return path.exists(path.join(self.image_path, self.images['background'])) and path.exists(path.join(self.image_path, self.images['target']))
 
     def _load_image_as_np(self, filename):
-        img = Image.open(path.join(IMAGE_PATH, filename))
+        img = Image.open(path.join(self.image_path, filename))
         return np.array(img)
 
     @property
@@ -95,13 +109,17 @@ class TestEnv(gym.Env):
                 done (boolean): whether the episode has ended, in which case further step() calls will return undefined results
                 info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
         """
+        # Increment time and check if game ends
         done = False
-        reward = 0
+        self.time += 1
+        if self.time > self.TIME_LIMIT:
+            done = True
 
+        reward = 0
         self.position[self.level] = action
         self.level += 1
 
-        if self.level >= self.image_power-1:
+        if self.level >= self.power-1:
             offx = 0
             offy = 0
             deltax = self.image_size
@@ -118,24 +136,42 @@ class TestEnv(gym.Env):
                 elif choice == 3:
                     offx += deltax
                     offy += deltay
-            val = self.state[offx, offy]
+            val = self.target[offx, offy]
             if val == 0:
-                if self.result[offx, offy] == 0:
+                if self.target[offx, offy] == 0.0:
                     reward = 1
                 else:
                     reward = -1
-                self.state[offx, offy] = reward
+                self.hits[offx, offy] = reward
             else:
                 reward == -1
             self.level = 0
             self.position.fill(0)
-        return np.array(self.state), reward, done, {}
+        
+        if self.miss_count > self.MISS_LIMIT:
+            done = True
+
+        # Assemble new observation and add cursor
+        observation = self.background #np.dstack([self.background, self.hits])
+        observation[self.hits > 0.0] = 200 
+
+        return observation, reward, done, {}
 
 
     def reset(self):
-        self.observation_space = self.background
-        self.state = np.zeros_like(self.result)
-        return np.array(self.state)
+        self.level = 0
+        self.position = np.zeros(self.power, dtype=np.uint32)
+
+        # Reset the non-static part of the observational space 
+        self.hits = np.zeros_like(self.background, dtype=float)
+        self.observation_space = np.dstack([self.background, self.hits]) 
+        
+        # Reset any counters 
+        self.miss_count = 0
+        self.time = 0
+        
+        return self.background
+        #return self.observation_space
 
     def render(self, mode='human', close=False):
         if close and self.viewer:
@@ -143,9 +179,36 @@ class TestEnv(gym.Env):
             self.viewer = None
             return
 
-        img = self.wallpaper
-        #ps = (self.state!=0)*255
-        #img = img + np.stack((ps, ps, ps, ps), axis=2)
+        # background is an rgba-array. We use this for rendering.
+        img = np.copy(self.wallpaper[:, :, :-1])
+        hit_mask = self.hits > 0.0
+        img[hit_mask, :] = 0
+        miss_mask = self.hits < 0.0
+        img[miss_mask, 0] = 255
+        img[miss_mask, 1] = 0        
+        img[miss_mask, 2] = 0
+
+        # create octree mask
+        offx = 0
+        offy = 0
+        deltax = self.image_size
+        deltay = self.image_size
+        level = 0
+        for choice in self.position:
+            level += 1
+            deltax = deltax//2
+            deltay = deltay//2
+            if choice == 0:
+                pass
+            elif choice == 1:
+                offx += deltax
+            elif choice == 2:
+                offy += deltay
+            elif choice == 3:
+                offx += deltax
+                offy += deltay
+            img[offx:(offx+deltax), offy:(offy+deltay), 0:3] = level%2*100+100
+
         if mode == 'rgb_array':
             return img
         elif mode == 'human':
